d8beebd616b56b8e670906641edee0d39f19dd47
Phumza kose <kosephumza@gmail.com>
2022-08-07 05:01:23 +0200

    Added a new file

--- dom.js
+++ dom.js
@@ -1,32 +1,29 @@
 const inputMessage = document.querySelector(".inputMessage");
-const inputMessage2 = document.querySelector(".motiva")
+const inputMessage2 = document.querySelector(".motiva");
 const shareButton = document.querySelector(".shareButton");
 const messages = document.querySelector(".messages");
-const motiv = document.querySelector(".motiv")
+const motiv = document.querySelector(".motiv");
 const btns = document.querySelector(".btns");
 
+shareButton.addEventListener("click", function () {
+  const message = inputMessage.value;
 
-shareButton.addEventListener("click", function(){
-    const message = inputMessage.value;
-    
-    let list = document.createElement('li');
-    list.textContent = message;
-    messages.appendChild(list);
-
-    
+  let list = document.createElement("li");
+  list.textContent = message;
+  messages.appendChild(list);
 });
-shareButton.addEventListener("click",function(event){
-    event.preventDefault();
-    inputMessage.value="";
-})
-shareButton.addEventListener("click",function(event){
-    event.preventDefault();
-    inputMessage2.value="";
-})
-btns.addEventListener("click", function(){
-    const message2 = inputMessage2.value;
+shareButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  inputMessage.value = "";
+});
+shareButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  inputMessage2.value = "";
+});
+btns.addEventListener("click", function () {
+  const message2 = inputMessage2.value;
 
-    let list2 = document.createElement('li');
-    list2.textContent = message2;
-    motiv.appendChild(list2);
-})
+  let list2 = document.createElement("li");
+  list2.textContent = message2;
+  motiv.appendChild(list2);
+});
